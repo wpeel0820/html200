4689e9a0570d08f4c3346c5fe39db32970eeb3a4
wpeel0820 <159219481+wpeel0820@users.noreply.github.com>
2024-06-03 18:07:47 -0700

    Add files via upload

---
+++ lesson05/lesson5-challenge1/demo.js
@@ -0,0 +1,9 @@
+function squaredNumber(firstNumber) {
+    let input =  prompt ('Enter a number');
+    let number= input;
+    let squaredNumber = number * number;
+
+    let result = squaredNumber;
+
+    alert(result);
+}
